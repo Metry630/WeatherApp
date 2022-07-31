ba87976244e9d5a2f057c52f5d6b585a0f86218b
Oey Joshua Jodrian <joshuajodrian@gmail.com>
2022-07-31 21:03:18 +0800

    reformat code using prettier

--- script.js
+++ script.js
@@ -1,56 +1,63 @@
-window.onload = function(){
-    searchButton = document.getElementById('search')
-    searchBar = document.getElementById('cityName')
-    currentCity = document.getElementById('city')
-    weatherDesc = document.getElementById('desc')
-    currentTemp = document.getElementById('temp')
-    gif = document.getElementById('gif')
-    icon = document.getElementById('icon')
-    if(searchBar !== true){console.log('search bar not here')}
-    if(searchButton !== true){console.log('search button not here')}
-    if(currentCity !== true){console.log('current city not here')}
-    searchButton.addEventListener("click", () => {changeData(searchBar.value)})
-}
-async function currentWeather(city){
-    weather = await fetch("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=411e30ed6eee0e55bc7bf37ffebeac76")
-    .then(data => data.json())
-    return weather
-}
-async function getData(city){
-    fullData = await currentWeather(city)
-    description = fullData['weather'][0]['description']
-    tempCelsius = Math.floor((fullData['main']['temp'] - 273.15)*10)/10
-    imageCode = fullData['weather'][0]['icon']
+window.onload = function () {
+    searchButton = document.getElementById("search");
+    searchBar = document.getElementById("cityName");
+    currentCity = document.getElementById("city");
+    weatherDesc = document.getElementById("desc");
+    currentTemp = document.getElementById("temp");
+    gif = document.getElementById("gif");
+    icon = document.getElementById("icon");
+    searchButton.addEventListener("click", () => {
+      changeData(searchBar.value);
+    });
+  };
+  async function currentWeather(city) {
+    weather = await fetch(
+      "http://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&APPID=411e30ed6eee0e55bc7bf37ffebeac76"
+    ).then((data) => data.json());
+    return weather;
+  }
+  async function getData(city) {
+    fullData = await currentWeather(city);
+    description = fullData["weather"][0]["description"];
+    tempCelsius = Math.floor((fullData["main"]["temp"] - 273.15) * 10) / 10;
+    imageCode = fullData["weather"][0]["icon"];
     return {
-        desc: description,
-        temp: tempCelsius,
-        code: imageCode
+      desc: description,
+      temp: tempCelsius,
+      code: imageCode,
+    };
+  }
+  async function changeData(city) {
+    try {
+      info = await getData(city);
+      imgUrl = "http://openweathermap.org/img/wn/" + info["code"] + "@2x.png";
+      gifUrl =
+        "https://api.giphy.com/v1/gifs/translate?api_key=bb2006d9d3454578be1a99cfad65913d&s=" +
+        info["desc"];
+      currentCity.innerHTML = "City: " + city;
+      weatherDesc.innerHTML = "Current Weather: " + info["desc"];
+      currentTemp.innerHTML = "Current Temperature: " + info["temp"] + "°C";
+      icon.src = imgUrl;
+      fetchGif(info["desc"]);
+    } catch {
+      currentCity.innerHTML = "City not found!";
     }
-}
-async function changeData(city){
-    try{
-        info = await getData(city)
-        imgUrl = "http://openweathermap.org/img/wn/" + info['code'] + "@2x.png"
-        gifUrl = 'https://api.giphy.com/v1/gifs/translate?api_key=bb2006d9d3454578be1a99cfad65913d&s=' + info['desc']
-        currentCity.innerHTML = "City: " + city
-        weatherDesc.innerHTML = "Current Weather: " + info['desc']
-        currentTemp.innerHTML = "Current Temperature: " + info['temp'] + "°C"
-        icon.src = imgUrl
-        fetchGif(info['desc'])
-    }catch{
-        currentCity.innerHTML = 'City not found!'
-    }
-}
-async function fetchGif(desc){
-    fetch('https://api.giphy.com/v1/gifs/translate?api_key=bb2006d9d3454578be1a99cfad65913d&s=' + desc, {mode: 'cors'})
-    .then(function(response) {
-      return response.json()
-    })
-    .then(function(response) {
-      gif.src = response.data.images.original.url
-    })
-    .catch(e => {
-      console.log(e)
-    })
-}
-
+  }
+  async function fetchGif(desc) {
+    fetch(
+      "https://api.giphy.com/v1/gifs/translate?api_key=bb2006d9d3454578be1a99cfad65913d&s=" +
+        desc,
+      { mode: "cors" }
+    )
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (response) {
+        gif.src = response.data.images.original.url;
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
